b3eb451fd99f50d366df5e56a8add7d334bf5c01
Manas Ranjan Mallick <manas1104@gmail.com>
2019-12-02 22:58:25 +1000

    Finished xylophone and started quizzler

--- xylophone-flutter-master/lib/main.dart
+++ xylophone-flutter-master/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +8,54 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text('Xylophone'),
         ),
+        body: play(),
+      ),
+    );
+  }
+}
+
+class play extends StatefulWidget {
+  @override
+  _playState createState() => _playState();
+}
+
+class _playState extends State<play> {
+  void playsound(int number) {
+    final player = AudioCache();
+    print(number);
+    player.play('note$number.wav');
+  }
+
+  Expanded buildkey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playsound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildkey(color: Colors.red, soundNumber: 7),
+          buildkey(color: Colors.orange, soundNumber: 6),
+          buildkey(color: Colors.yellow, soundNumber: 5),
+          buildkey(color: Colors.green, soundNumber: 4),
+          buildkey(color: Colors.blue, soundNumber: 3),
+          buildkey(color: Colors.indigo, soundNumber: 2),
+          buildkey(color: Colors.purple, soundNumber: 1),
+        ],
       ),
     );
   }
